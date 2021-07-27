7fe70313ddb6280d01e631c3e74321bf0a49708a
Tara Z. Manicsic <tzmanics@gmail.com>
2021-07-27 00:27:48 -0400

    adds product list component and product model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'product-list', loadChildren: () => import('./product-list/product-list.module').then(m => m.ProductListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
