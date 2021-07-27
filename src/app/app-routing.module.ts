742aa8af8e8aaf49bd55ee406f1a4b6712125411
tzmanics <tzmanics@gmail.com>
2021-07-27 10:41:16 -0400

    adds netlify setup & route formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'product-list', loadChildren: () => import('./product-list/product-list.module').then(m => m.ProductListModule) }];
+const routes: Routes = [
+  {
+    path: 'product-list',
+    loadChildren: () =>
+      import('./product-list/product-list.module').then(
+        (m) => m.ProductListModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
