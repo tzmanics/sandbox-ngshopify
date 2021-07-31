a2ca4837396bb9e616ed424f6fbf9de8a0cc9af7
Tara Z. Manicsic <tzmanics@gmail.com>
2021-07-30 22:41:28 -0400

    adds reroute & product-list page template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'product-list',
+    pathMatch: 'full',
+  },
+  {
     path: 'product-list',
     loadChildren: () =>
       import('./product-list/product-list.module').then(
